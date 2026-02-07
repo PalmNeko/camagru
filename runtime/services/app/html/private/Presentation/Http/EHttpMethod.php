3d39c51b32dc436d2288a0d9f80ab1dbddd18217
PalmNeko <nitunamoni13579@gmail.com>
2026-02-07 05:16:45 +0000

    add: EHttpMethod and EHttpMethod.

---
+++ runtime/services/app/html/private/Presentation/Http/EHttpMethod.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace PalmNeko\Camagru\Presentation\Http;
+
+enum EHttpMethod: string
+{
+    case Get = 'GET';
+    case Post = 'POST';
+    case Put = 'PUT';
+    case Delete = 'DELETE';
+
+    public static function from_string($data) : ?self {
+        $s = serialize(...);
+        return match(true) {
+            strcasecmp("Get", $data) === 0 => self::Get,
+            strcasecmp("Post", $data) === 0 => self::Post,
+            strcasecmp("Put", $data) === 0 => self::Put,
+            strcasecmp("Delete", $data) === 0 => self::Delete,
+            default => null,
+        };
+    }
+}
