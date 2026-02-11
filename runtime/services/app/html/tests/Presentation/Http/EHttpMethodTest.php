921cdacbaab4b64c64306e2bb5d1039fc4766213
PalmNeko <nitunamoni13579@gmail.com>
2026-02-11 19:33:29 +0000

    update

--- runtime/services/app/html/tests/Presentation/Http/EHttpMethodTest.php
+++ runtime/services/app/html/tests/Presentation/Http/EHttpMethodTest.php
@@ -2,21 +2,19 @@
 
 use PalmNeko\Camagru\Presentation\Http\EHttpMethod;
 
+pest()->group('EHttpMethod');
+
 describe('EHttpMethod', function () {
-    describe('::from_string', function () {
-        it('HappyPath', function () {
-            expect(EHttpMethod::from_string('GET'))->toBe(EHttpMethod::Get);
-            expect(EHttpMethod::from_string('POST'))->toBe(EHttpMethod::Post);
-            expect(EHttpMethod::from_string('PUT'))->toBe(EHttpMethod::Put);
-            expect(EHttpMethod::from_string('DELETE'))->toBe(EHttpMethod::Delete);
-        });
-        it('InvalidUsage', function () {
-            expect(EHttpMethod::from_string(''))->toBe(null);
-            expect(EHttpMethod::from_string('HEAD'))->toBe(null);
-            expect(EHttpMethod::from_string('CONNECT'))->toBe(null);
-            expect(EHttpMethod::from_string('OPTIONS'))->toBe(null);
-            expect(EHttpMethod::from_string('TRACE'))->toBe(null);
-            expect(EHttpMethod::from_string('PATCH'))->toBe(null);
-        });
+    test('::from_string', function () {
+        expect(EHttpMethod::from_string('GET'))->toBe(EHttpMethod::Get);
+        expect(EHttpMethod::from_string('POST'))->toBe(EHttpMethod::Post);
+        expect(EHttpMethod::from_string('PUT'))->toBe(EHttpMethod::Put);
+        expect(EHttpMethod::from_string('DELETE'))->toBe(EHttpMethod::Delete);
+        expect(EHttpMethod::from_string(''))->toBe(null);
+        expect(EHttpMethod::from_string('HEAD'))->toBe(null);
+        expect(EHttpMethod::from_string('CONNECT'))->toBe(null);
+        expect(EHttpMethod::from_string('OPTIONS'))->toBe(null);
+        expect(EHttpMethod::from_string('TRACE'))->toBe(null);
+        expect(EHttpMethod::from_string('PATCH'))->toBe(null);
     });
 });
--- runtime/services/app/html/tests/Presentation/Http/RequestHandlerRuleTest.php
+++ runtime/services/app/html/tests/Presentation/Http/RequestHandlerRuleTest.php
@@ -3,51 +3,36 @@
 use PalmNeko\Camagru\Presentation\Http\{
     EHttpMethod,
     RequestHandlerRule,
-    HandlerResult, IRequestHandler,
+    HandlerResult,
 };
-
-class TempHandler implements IRequestHandler {
-    public function __construct(
-        public HandlerResult $handlerResult
-    ) {}
-
-    public function invoke() : HandlerResult {
-        return $this->handlerResult;
-    }
-}
+use Tests\Presentation\Http\TempHandler;
 
 describe('RequestHandlerRuleTest', function () {
-    describe('::add(get,post,put,delete)', function () {
-        test('HappyPath', function () use (& $rule) {
-            $pattern = '/\/.*/';
-            $response = new HandlerResult(false);
-            $rule = RequestHandlerRule::add(
-                EHttpMethod::Get,
-                $pattern,
-                new TempHandler($response));
-            expect($rule->pattern)->toBe($pattern);
-            expect($rule->method)->toBe(EHttpMethod::Get);
-            expect($rule->handler->invoke())->toBe($response);
-        });
+    test('::add(get,post,put,delete)', function () {
+        $pattern = '/\/.*/';
+        $response = new HandlerResult(false);
+        $rule = RequestHandlerRule::add(
+            EHttpMethod::Get,
+            $pattern,
+            new TempHandler($response));
+        expect($rule->pattern)->toBe($pattern);
+        expect($rule->method)->toBe(EHttpMethod::Get);
+        expect($rule->handler->invoke())->toBe($response);
     });
-    describe('->isMatch', function () {
+    test('->isMatch', function () {
         $pattern = '/^\/path\/.+\/image/';
         $response = new HandlerResult(false);
         $rule = RequestHandlerRule::get($pattern, new TempHandler($response));
-        test('HappyPath', function () use (& $rule) {
-            expect($rule->isMatch(EHttpMethod::Get, '/path/abc/image'))->toBeTrue();
-            expect($rule->isMatch(EHttpMethod::Get, '/path/abc/image/'))->toBeTrue();
-            expect($rule->isMatch(EHttpMethod::Get, '/path/abc/image/hij'))->toBeTrue();
-            expect($rule->isMatch(EHttpMethod::Get, '/path/abc/def/image'))->toBeTrue();
-        });
-        test('InvalidPath', function () use (& $rule, & $pattern) {
-            expect($rule->isMatch(EHttpMethod::Post, '/path/abc/image'))->toBeFalse();
-
-            expect($rule->isMatch(EHttpMethod::Get, $pattern))->toBeFalse();
-            expect($rule->isMatch(EHttpMethod::Get, '^/path/.+/image'))->toBeFalse();
-            expect($rule->isMatch(EHttpMethod::Get, '/path//image'))->toBeFalse();
-            expect($rule->isMatch(EHttpMethod::Get, ' /path/abc/image'))->toBeFalse();
-            expect($rule->isMatch(EHttpMethod::Get, 'invalid-root/path/abc/image'))->toBeFalse();
-        });
+        expect($rule->isMatch(EHttpMethod::Get, '/path/abc/image'))->toBeTrue();
+        expect($rule->isMatch(EHttpMethod::Get, '/path/abc/image/'))->toBeTrue();
+        expect($rule->isMatch(EHttpMethod::Get, '/path/abc/image/hij'))->toBeTrue();
+        expect($rule->isMatch(EHttpMethod::Get, '/path/abc/def/image'))->toBeTrue();
+        // will be false
+        expect($rule->isMatch(EHttpMethod::Post, '/path/abc/image'))->toBeFalse();
+        expect($rule->isMatch(EHttpMethod::Get, $pattern))->toBeFalse();
+        expect($rule->isMatch(EHttpMethod::Get, '^/path/.+/image'))->toBeFalse();
+        expect($rule->isMatch(EHttpMethod::Get, '/path//image'))->toBeFalse();
+        expect($rule->isMatch(EHttpMethod::Get, ' /path/abc/image'))->toBeFalse();
+        expect($rule->isMatch(EHttpMethod::Get, 'invalid-root/path/abc/image'))->toBeFalse();
     });
 });
