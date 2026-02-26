2a9450226f851fbbbf7cc3ac72d0aa8628df1d14
PalmNeko <nitunamoni13579@gmail.com>
2026-02-26 08:33:55 +0000

    add: InMemoryStorageClient

---
+++ runtime/services/app/html/private/Infrastructure/InMemory/InMemoryStorageClient.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PalmNeko\Camagru\Infrastructure\Repository\InMemory;
+
+class InMemoryStorageClient
+{
+    public function __construct(
+        public private(set) array $data,
+    ) {}
+
+    public function append(mixed $value) {
+        $this->data[] = $value;
+    }
+
+    public function equal(self $other) : bool {
+        return $this->data === $other->data;
+    }
+
+    public function backup() : self {
+        return unserialize(serialize($this));
+    }
+
+    public function rollback(self $backup) {
+        $this->data = $backup->data;
+    }
+}
---
+++ runtime/services/app/html/private/Infrastructure/InMemory/InMemoryStorageClientTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use PalmNeko\Camagru\Infrastructure\Repository\InMemory\InMemoryStorageClient;
+
+describe('InMemoryStorageClient', function () {
+    test('->append()', function () {
+        $storage = new InMemoryStorageClient(['1', '2']);
+        $storage->append('3');
+        expect($storage->data)->toBe(['1', '2', '3']);
+    });
+
+    test('->backup(), ->rollback()', function () {
+        $storage = new InMemoryStorageClient(['1', '2']);
+        $backup = $storage->backup();
+        expect($backup->data)->toBe(['1', '2']);
+        $storage->append('3');
+        $storage->rollback($backup);
+        expect($storage->data)->toBe(['1', '2']);
+    });
+
+    test('->equal()', function () {
+        $storage1 = new InMemoryStorageClient(['1', '2']);
+        $storage2 = new InMemoryStorageClient(['1', '2']);
+        expect($storage1->equal($storage2))->toBeTrue();
+    });
+});
